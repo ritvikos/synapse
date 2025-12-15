9849d573da1238378b18d071cb0e91246fcd9419
Ritvik Gupta <ritvikfoss@gmail.com>
2025-12-15 05:48:48 +0530

    test(spooler): Add basic tests
    - Ensure that the basic functionality works:
        Create File -> Write Chunks -> Commit
    
    - Add the following dependencies for testing:
        - github.com/brianvoe/gofakeit/v7
        - github.com/stretchr/testify
    
    Signed-off-by: Ritvik Gupta <ritvikfoss@gmail.com>

--- go.mod
+++ go.mod
@@ -5,10 +5,17 @@ go 1.24.9
 require (
 	github.com/Workiva/go-datastructures v1.1.7
 	github.com/andybalholm/brotli v1.2.0
+	github.com/brianvoe/gofakeit/v7 v7.12.1
 	github.com/klauspost/compress v1.18.1
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d
 	github.com/temoto/robotstxt v1.1.2
 	golang.org/x/net v0.46.0
 )
 
-require golang.org/x/text v0.30.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	golang.org/x/text v0.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ spooler/spooler_test.go
@@ -0,0 +1,110 @@
+package spooler
+
+import (
+	"math/rand/v2"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+// TODO: Write more robust tests once spooler's internal implementation is completed, till then, basic ones.
+
+func generateTestData(t *testing.T) string {
+	t.Helper()
+
+	paragraphCount := int(rand.Int64N(15))
+	sentenceCount := int(rand.Int64N(20))
+	wordCount := int(rand.Int64N(100))
+	separation := " "
+
+	return gofakeit.LoremIpsumParagraph(paragraphCount, sentenceCount, wordCount, separation)
+}
+
+func generateTestChunks(t *testing.T, numChunks int) [][]byte {
+	t.Helper()
+
+	chunks := make([][]byte, numChunks)
+	for i := range numChunks {
+		chunks[i] = []byte(generateTestData(t))
+	}
+	return chunks
+}
+
+func writeChunks(t *testing.T, spooler *Spooler, chunks [][]byte) int {
+	t.Helper()
+
+	totalSize := 0
+	for _, chunk := range chunks {
+		err := spooler.WriteChunk(chunk)
+		require.NoError(t, err, "failed to write chunk")
+		totalSize += len(chunk)
+	}
+	return totalSize
+}
+
+func TestSpoolerSync(t *testing.T) {
+	wd, err := os.Getwd()
+	require.NoError(t, err, "failed to get working directory")
+
+	baseDir := filepath.Join(wd, "test_spooler")
+
+	config := SpoolerConfig{
+		BatchConfig: BatchConfig{
+			BaseDir:      baseDir,
+			MaxBatchSize: 2 * 1024 * 1024,
+			Processor: BatchProcessor{
+				Async:        false,
+				DeleteSource: true,
+				Hooks:        nil,
+			},
+		},
+		FileWriterConfig: FileWriterConfig{
+			MaxFileSize: 5 * 1024 * 1024,
+		},
+	}
+
+	spooler, err := NewSpooler(config)
+	require.NoError(t, err, "failed to create spooler")
+
+	t.Cleanup(func() {
+		time.Sleep(10 * time.Second)
+		os.RemoveAll(baseDir)
+	})
+
+	t.Run("write single file", func(t *testing.T) {
+		ctx := t.Context()
+
+		err := spooler.NewWriter(ctx, "testfile1.txt")
+		require.NoError(t, err, "failed to create new writer")
+
+		chunks := generateTestChunks(t, 3)
+		totalSize := writeChunks(t, spooler, chunks)
+
+		err = spooler.Commit()
+		require.NoError(t, err, "failed to commit")
+
+		assert.Equal(t, totalSize, spooler.BatchSize(), "batch size mismatch")
+	})
+
+	t.Run("write multiple files", func(t *testing.T) {
+		ctx := t.Context()
+		initialBatchSize := spooler.BatchSize()
+
+		err := spooler.NewWriter(ctx, "testfile2.txt")
+		require.NoError(t, err, "failed to create new writer")
+
+		chunks := generateTestChunks(t, 5)
+		totalSize := writeChunks(t, spooler, chunks)
+
+		err = spooler.Commit()
+		require.NoError(t, err, "failed to commit")
+
+		expectedBatchSize := initialBatchSize + totalSize
+		assert.Equal(t, expectedBatchSize, spooler.BatchSize(), "batch size mismatch after second file")
+	})
+}
